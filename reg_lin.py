7fab7b73b93130c25283f40ecf8f031c43a37090
Ariel Montoya <ariel_montoya15@outlook.es>
2020-04-01 13:23:10 -0600

    fix

--- reg_lin.py
+++ reg_lin.py
@@ -6,15 +6,13 @@ def read_data(file):
 	f = open(file)
 	data = f.read().split()[1:]
 	f.close()
-
 	mat = []
 	for row in data:
-		mat.append(np.insert(np.array(row.split(",")[2:]).astype(np.float),0,1))
+		mat.append(np.array(row.split(",")[2:]).astype(np.float))
 	return mat
 
 def shuffle(data):
 	random.shuffle(data,random.random)
-	#print(type(aux))
 	lim = int(len(data)*0.7)
 	train = data[:lim]
 	test = data[lim:]
@@ -23,44 +21,42 @@ def shuffle(data):
 def mat_x(mat):
 	xs = []
 	for row in mat:
-		xs.append((row[:-1]))
-	return np.array(np.matrix(xs))
+		xs.append(np.insert(row[1:],0,1))
+	return np.array(xs)
 
 def mat_y(mat):
 	y = []
 	for row in mat:
-		y.append(row[-1])
+		y.append(row[0])
 	return np.array(y)
 
 
 def f_scaling(mat_x):
 	sca = []
-	sca.append(mat_x[0])
-	for i in range(1,len(mat_x)):
+	for i in range(len(mat_x)):
 		x = mat_x[i]
 		miu = np.average(x)
 		si = np.std(x)
-		#print(i,si,type(si))
 		x = (x - miu) / si
 		sca.append(x)
 	return np.array(sca)
 
-def cost(x,y,a,n,m):
-	pass
-
-def hyp(x,n,theta):
-	su = 0
-	for i in range(n):
-		su = su + theta[i]*x[i]
-	return su
-
-def suma(x,theta,y,m,n,j):
+def cost(x,y,theta,m):
 	su = 0
 	for i in range(m):
-		su = np.round(su + (hyp(x[i],n,theta) - y[i])*x[i][j],decimals=8)
-	return np.round(su,decimals=8)
+		su = su + ((hyp(x[i],theta)-y[i])**2)
+	return (1/(2*m))*su
 
-def gradient(x,y,a,init,ite):
+def hyp(x,theta):
+	return x.dot(theta)
+
+def suma(x,theta,y,m,j):
+	su = 0
+	for i in range(m):
+		su = su + (hyp(x[i],theta) - y[i])*x[i][j]
+	return su
+
+def gradient(x,y,a = 0.1,init = 0,ite = 1000,show = 50):
 	m = len(x)
 	n = len(x[0])
 	theta = []
@@ -69,27 +65,29 @@ def gradient(x,y,a,init,ite):
 		theta.append(init)
 		aux.append(init)
 
-	print("init")
 	for c in range(ite):
-		print("ite",c)
-		print("theta",theta)
 		for j in range(n):
-			aux[j] = np.round(-(a/m) * suma(x,theta,y,m,n,j),decimals=8)
+			aux[j] = aux[j] - ((a/m) * suma(x,theta,y,m,j))
 		for j in range(n):
 			theta[j] = aux[j]
-	print("end")
+		if (c + 1)%show == 0:
+			print("cost at iteration",c+1,cost(x,y,theta,m))
 	return theta
 
 
 if __name__ == '__main__':
-	mat = read_data("prueba.txt")
+	mat = read_data("data.txt")
 	train, test = shuffle(mat)
-	x = mat_x(train)
-	y = mat_y(train)
-	#print(len(train))
-	#print()
-	#print(y)
-	xf = f_scaling(x)
-	#print(len(xf))
-	thetha = gradient(x,y,0.1,0,50)
-	print(thetha)
+	x_train = mat_x(train)
+	y_train = mat_y(train)
+	x_train = f_scaling(x_train)
+	theta = gradient(x_train,y_train)
+	print(theta)
+
+	x_test = mat_x(test)
+	y_test = mat_y(test)
+	f = open("test.txt","w")
+	for i in range(len(x_test)):
+		h = hyp(x_test[i],theta)
+		f.write("hypothesis: "+ str(h)+" correct: "+str(y_train[i])+"\n")
+	f.close()
